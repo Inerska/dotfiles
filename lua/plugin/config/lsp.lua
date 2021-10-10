282348271ea408ec3fca7ca81e4b060b0c3e1396
Inerska <contact@agdl.dev>
2021-10-10 09:20:56 +0200

    feat: save

--- lua/plugin/config/lsp.lua
+++ lua/plugin/config/lsp.lua
@@ -95,6 +95,17 @@ local function make_config()
     }
 end
 
+vim.notify = function(msg, log_level, _opts)
+    if msg:match("exit code") then
+        return
+    end
+    if log_level == vim.log.levels.ERROR then
+        vim.api.nvim_err_writeln(msg)
+    else
+        vim.api.nvim_echo({{msg}}, true, {})
+    end
+end
+
 
 local lsp_installer = require("nvim-lsp-installer")
 
