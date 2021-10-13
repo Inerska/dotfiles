cf1bf66c67b998581a3929a69bdff72f796f9786
Inerska <contact@agdl.dev>
2021-10-13 21:22:57 +0200

    feat(cmp): tab to select next item

--- lua/plugin/config/cmp.lua
+++ lua/plugin/config/cmp.lua
@@ -41,32 +41,28 @@ cmp.setup({
             select = true,
         }),
         ["<Tab>"] = cmp.mapping(function(fallback)
-            if vim.fn.pumvisible() == 1 then
-                vim.fn.feedkeys(t("<C-n>"), "n")
+            if cmp.visible() then
+                cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then
-                vim.fn.feedkeys(t("<Plug>luasnip-expand-or-jump"), "")
-            elseif check_back_space() then
-                vim.fn.feedkeys(t("<tab>"), "n")
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
             else
                 fallback()
             end
-        end, {
-        "i",
-        "s",
-    }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if vim.fn.pumvisible() == 1 then
-            vim.fn.feedkeys(t("<C-p>"), "n")
-        elseif luasnip.jumpable(-1) then
-            vim.fn.feedkeys(t("<Plug>luasnip-jump-prev"), "")
-        else
-            fallback()
-        end
-    end, {
-    "i",
-    "s",
-}),
-        },
+        end, { "i", "s" }
+        ),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }
+        ),      },
 
         -- You should specify your *installed* sources.
         sources = {
