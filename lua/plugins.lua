53f43c1521811d9d60213acdc93ede3daba12da6
Inerska <contact@agdl.dev>
2021-10-15 18:29:14 +0200

    feat: add job split for async jobs divide

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -231,6 +231,8 @@ return require('packer').startup(function()
 
     use 'kosayoda/nvim-lightbulb'
 
+    use 'Groctel/jobsplit.vim'
+
     vim.api.nvim_set_keymap("n", "gk", "<cmd>Spotify play/pause<CR>", { noremap = true, silent = true })
     vim.api.nvim_set_keymap("n", "gl", "<cmd>Spotify next<CR>", { noremap = true, silent = true })
     vim.api.nvim_set_keymap("n", "gh", "<cmd>Spotify prev<CR>", { noremap = true, silent = true })
